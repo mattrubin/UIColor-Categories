225ab9c1e7df81ad0fc3967b5ce46562ba87584e
Matt Rubin <mattrubin@me.com>
2013-12-22 23:49:07 -0500

    Remove stubbed UIColor_Categories class

--- UIColor+Categories/UIColor+Categories.h
+++ UIColor+Categories/UIColor+Categories.h
@@ -1,13 +1,9 @@
 //
-//  UIColor_Categories.m
+//  UIColor+Categories.h
 //  UIColor+Categories
 //
 //  Created by Matt Rubin on 12/22/13.
 //  Copyright (c) 2013 Matt Rubin. All rights reserved.
 //
 
-#import "UIColor_Categories.h"
-
-@implementation UIColor_Categories
-
-@end
+#import <Foundation/Foundation.h>
