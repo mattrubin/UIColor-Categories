4decd59b4ad71b6387a18e9503f3d5a43bc7c396
Matt Rubin <mattrubin@me.com>
2013-12-22 20:23:09 -0500

    New static library

---
+++ UIColor+Categories/UIColor_Categories.m
@@ -0,0 +1,13 @@
+//
+//  UIColor_Categories.m
+//  UIColor+Categories
+//
+//  Created by Matt Rubin on 12/22/13.
+//  Copyright (c) 2013 Matt Rubin. All rights reserved.
+//
+
+#import "UIColor_Categories.h"
+
+@implementation UIColor_Categories
+
+@end
---
+++ UIColor+CategoriesTests/UIColor_CategoriesTests.m
@@ -0,0 +1,34 @@
+//
+//  UIColor_CategoriesTests.m
+//  UIColor+CategoriesTests
+//
+//  Created by Matt Rubin on 12/22/13.
+//  Copyright (c) 2013 Matt Rubin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIColor_CategoriesTests : XCTestCase
+
+@end
+
+@implementation UIColor_CategoriesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
