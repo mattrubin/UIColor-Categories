9c130030278c51ead3c379c84eefbd2d5bf28a09
Matt Rubin <mattrubin@me.com>
2013-12-23 01:01:29 -0500

    Add rudimentary colorWithInteger… tests

---
+++ UIColor+CategoriesTests/UIColorIntegerTests.m
@@ -0,0 +1,26 @@
+//
+//  UIColorIntegerTests.m
+//  UIColor+Categories
+//
+//  Created by Matt Rubin on 12/23/13.
+//  Copyright (c) 2013 Matt Rubin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "UIColor+Integers.h"
+
+
+@interface UIColorIntegerTests : XCTestCase
+
+@end
+
+
+@implementation UIColorIntegerTests
+
+- (void)testRGB
+{
+    XCTAssertEqualObjects([UIColor colorWithIntegerRed:51 green:102 blue:153 alpha:50],
+                          [UIColor colorWithRed:0.2 green:0.4 blue:0.6 alpha:0.5]);
+}
+
+@end
